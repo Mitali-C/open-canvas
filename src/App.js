fcf7a9d54f2a74d8434b3bf8b077237338325484
Mitali Chaudhari <mitali.chaudhari98@gmail.com>
2021-05-20 13:19:54 +0530

    basic triangle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import Canvas from './components/canvas/Canvas';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.scss';
+import WebglCanvas from './components/webgl-canvas/WebglCanvas';
+import Elements from './components/webgl-elements/Elements';
 
 class App extends React.Component {
   render(){
     return(
-      <div>
-        <Canvas></Canvas>
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/elements">
+            <Elements></Elements>
+          </Route>
+          <Route path="/webgl">
+            <WebglCanvas></WebglCanvas>
+          </Route>
+          <Route
+            path='/'
+            component={Canvas}
+          />
+        </Switch>
+      </Router>
     )
   }
 };
