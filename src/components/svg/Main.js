119c99da7044323a20772b209e12b3c48b0f6329
Mitali Chaudhari <mitali.chaudhari98@gmail.com>
2021-06-16 17:47:30 +0530

    Markup. TODO: MARKUP

--- src/components/svg/Main.js
+++ src/components/svg/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import circle_icon from '../../assets/icons/circle.png';
 import rectangle_icon from '../../assets/icons/rectangle.png';
-// import pen_icon from '../../assets/icons/pen.png'; 
+import pen_icon from '../../assets/icons/pen.png'; 
 import select_icon from '../../assets/icons/cursor.png';
 // import image_icon from '../../assets/icons/image_icon.png';
 // import text_icon from '../../assets/icons/text.png';
@@ -9,6 +9,7 @@ import select_icon from '../../assets/icons/cursor.png';
 import './main.scss';
 import Rectangle from './Rectangle';
 import { v4 as uuidv4 } from 'uuid';
+import Path from './Path';
 
 class Main extends React.Component{
   state = {
@@ -17,7 +18,9 @@ class Main extends React.Component{
     rectangles: [
 
     ],
-    selected_id:''
+    selected_id:'',
+    paths:[],
+    isDrawing:false,
   }
 
   selectId = (id) => {
@@ -35,19 +38,27 @@ class Main extends React.Component{
       }}>
         <img src={rectangle_icon} alt="rectangle"></img>
       </div>
+      <div className="icon-container" onClick={()=>{this.setState({tool:'pen'})}}>
+        <img src={pen_icon} alt="pen"></img>
+      </div>
     </div>
     )
   }
 
   onMouseDown = (e) => {
-    this.setState({selected_id:''});
+    // this.setState({selected_id:''});
     switch(this.state.tool){
+      case 'pen':
+        this.setState({isDrawing:true});
+        let tempPaths = this.state.paths;
+        tempPaths.push({x:e.pageX, y: e.pageY, id:uuidv4(), points:[{x:e.pageX, y:e.pageY }]});
+        break;
       case 'shape':
         switch(this.state.shape_type){
           case 'rectangle':
             let tempRects = this.state.rectangles;
             tempRects.push({x:e.pageX, y: e.pageY, width:100, height:100, id:uuidv4()});
-            this.setState({rectangles: tempRects, tool:'select'});
+            this.setState({rectangles: tempRects});
             break;
           default:
             break;
@@ -58,14 +69,56 @@ class Main extends React.Component{
     }
   }
 
+  getCursorType = () => {
+    switch(this.state.tool){
+      case 'select':
+        return 'pointer'
+      case 'pen':
+        return 'pointer'
+      case 'eraser':
+        return 'pointer'
+      case 'shape':
+        return 'crosshair'
+      default:
+        return 'pointer'
+    }
+  }
+
+  onMouseUp = () => {
+    this.setState({isDrawing:false})
+  }
+
+  onMouseMove = (e) => {
+    if(this.state.isDrawing){
+      let paths = this.state.paths;
+      const x = e.pageX;
+      const y = e.pageY;
+      let temp = paths[paths.length-1];
+      let temp_points=[];
+      if(temp.points){
+        temp_points = temp.points;
+      }
+      temp_points.push({x,y});
+      temp.points = temp_points;
+      paths.pop();
+      paths.push(temp);
+      this.setState({paths})
+    }
+  }
+
   render(){
     return(
       <div id="svg-main">
         {this.renderToolbar()}
-        <svg style={{minHeight:'100vh', width:'100%'}} id="svg-container"  onMouseDown={this.onMouseDown}>
+        <svg style={{minHeight:'100vh', width:'100%', cursor:this.getCursorType()}} id="svg-container"  onMouseDown={this.onMouseDown} onMouseUp={this.onMouseUp} onMouseMove={this.onMouseMove}>
           {
             this.state.rectangles.map((rectangle, index) => (
               <Rectangle data={rectangle} key="index" selectId={this.selectId} selected_id={this.state.selected_id}></Rectangle>
+            ))
+          }
+          {
+            this.state.paths.map(path => (
+              <Path data={path} selectId={this.selectId} selected_id={this.state.selected_id}></Path>
             ))
           }
         </svg>
---
+++ src/components/svg/Path.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import interact from 'interactjs';
+
+class Path extends React.Component{
+  state = {
+    width:0,
+    height:0,
+    x:0, 
+    y:0,
+    id:'',
+    selected_id:'',
+    selected: true
+  }
+  static getDerivedStateFromProps(props, state) {
+    if(props.selected_id !== state.selected_id){
+        //Change in props
+        return{
+          selected_id: props.selected_id
+        };
+    }
+    return null; // No change to state
+  }
+
+  componentDidUpdate(prevState, prevProps) {
+    // if(this.props.selected_id!==prevState.selected_id && this.props.selected_id!==this.state.id){
+    //   this.setState({selected:false});
+    //   this.removeTransformer();
+    // }
+  }
+
+  componentDidMount(){
+    // console.log(this.props.data);
+    this.setState({id:this.props.data.id})
+  }
+
+  onClick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    this.props.selectId(this.state.id);
+    var myPathBox = document.getElementsByClassName(`drag-svg-${this.state.id}`)[0].getBBox();
+    if(myPathBox){
+      this.setState({x:myPathBox.x, y:myPathBox.y, width:myPathBox.width, height:myPathBox.height})
+    }
+    const {selected_id} = this.props;
+    if(selected_id!==this.state.id){
+      this.props.selectId(this.state.id);
+      // this.addTransformer();
+    }
+  }
+
+  dragMoveListener = (event) =>{
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the position attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+    this.setState({x: event.rect.left, y: event.rect.top})
+  }
+
+  addTransformer = () => {
+    const {data} = this.props;
+    interact(`.drag-svg-bounds-${data.id}`)
+    .draggable({
+      onmove: this.dragMoveListener
+    })
+    .resizable({
+      preserveAspectRatio: false,
+      edges: { left: true, right: true, bottom: true, top: true }
+    })
+    .on('resizemove',  (event) => {
+      var target = event.target,
+          x = (parseFloat(target.getAttribute('data-x')) || 0),
+          y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+      // update the element's style
+      target.style.width  = event.rect.width + 'px';
+      target.style.height = event.rect.height + 'px';
+
+      // translate when resizing from top or left edges
+      x += event.deltaRect.left;
+      y += event.deltaRect.top;
+
+      target.style.webkitTransform = target.style.transform =
+          'translate(' + x + 'px,' + y + 'px)';
+
+      target.setAttribute('data-x', x);
+      target.setAttribute('data-y', y);
+      target.textContent = event.rect.width + '×' + event.rect.height;
+      this.setState({x: this.state.x+event.deltaRect.left, y: this.state.y+event.deltaRect.top, width:event.rect.width, height: event.rect.height})
+    });
+  }
+
+  render(){
+    const {data} = this.props;let path = '';
+    let points = data.points.slice(0);
+    if (points.length > 0) {
+      path = `M ${points[0].x} ${points[0].y}`;
+      var p1, p2, end;
+      for (var i = 1; i < points.length - 2; i += 2) {
+        p1 = points[i];
+        p2 = points[i + 1];
+        end = points[i + 2];
+        path += ` C ${p1.x} ${p1.y}, ${p2.x} ${p2.y}, ${end.x} ${end.y}`;
+      }
+    }
+    return(
+      <g>
+        <path id={this.state.id} className={`drag-svg-${this.state.id}`} key={path} stroke="blue" strokeWidth={2} d={path} fill="none" onMouseDown={this.onClick}></path>
+        {
+          this.state.id === this.props.selected_id && (
+            <>
+            <rect className={`drag-svg-bounds-${this.state.id}`} x={this.state.x} y={this.state.y} width={this.state.width} height={this.state.height} stroke="black" fill="transparent" stroke-width="1" onClick={this.click}></rect>
+            {/* connecting lines */}
+            <line x1={this.state.x-5} y1={this.state.y-5} x2={this.state.x+this.state.width+5} y2={this.state.y-5}  style={{stroke:'#6cb7ff', strokeWidth:1}} ></line>
+            <line x1={this.state.x-5} y1={this.state.y-5} x2={this.state.x-5} y2={this.state.y+this.state.height+5}  style={{stroke:'#6cb7ff', strokeWidth:1}} ></line>
+            <line x1={this.state.x+this.state.width+5} y1={this.state.y-5} x2={this.state.x+this.state.width+5} y2={this.state.y+this.state.height+5}  style={{stroke:'#6cb7ff', strokeWidth:1}} ></line>
+            <line x1={this.state.x-5} y1={this.state.y+this.state.height+5} x2={this.state.x+this.state.width+5} y2={this.state.y+this.state.height+5}  style={{stroke:'#6cb7ff', strokeWidth:1}} ></line>
+            {/* top left */}
+            <circle cx={this.state.x-5} cy={this.state.y-5} r="5" stroke="#6cb7ff" stroke-width="1" fill="#FFF" />
+            {/* bottom right */}
+            <circle cx={this.state.x+this.state.width+5} cy={this.state.y+this.state.height+5} r="5" stroke="#6cb7ff" stroke-width="1" fill="#FFF" />
+            {/* top right */}
+            <circle cx={this.state.x+this.state.width+5} cy={this.state.y-5} r="5" stroke="#6cb7ff" stroke-width="1" fill="#FFF" />
+            {/* bottom left */}
+            <circle cx={this.state.x-5} cy={this.state.y+this.state.height+5} r="5" stroke="#6cb7ff" stroke-width="1" fill="#FFF" />
+            </>
+          )
+        }
+      </g>
+    )
+  }
+}
+
+export default Path;
